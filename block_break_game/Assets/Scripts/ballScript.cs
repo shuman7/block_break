30bee471191d097be4b03c15c9a03867a9fa6e4e
shuman7 <31026019+shuman7@users.noreply.github.com>
2020-10-13 15:28:34 +0900

    ゲームスタート時にボールが動く実装

---
+++ block_break_game/Assets/Scripts/ballScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballScript : MonoBehaviour
+{
+    public float ballSpeed = 30.0f;
+    private Rigidbody myRigid;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigid = this.GetComponent<Rigidbody>();
+        myRigid.AddForce((transform.forward + transform.right) * ballSpeed, ForceMode.VelocityChange);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
